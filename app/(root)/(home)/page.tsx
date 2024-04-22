6c3576671e2f6c6b9b1426943f0e58442d46be7b
M Rizki Ramadhan <m.rizki.ramadhan@wgs-id.com>
2024-04-22 15:53:17 +0700

    Feat(Homepage): Create component for homepage and reuseable modal

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import MeetingTypeList from "@/components/MeetingTypeList";
 
 function Home() {
+  // Time set
+  const now = new Date();
+  const time = now.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = now.toLocaleDateString([], {
+    day: "numeric",
+    month: "long",
+    weekday: "long",
+    year: "numeric",
+  });
   return (
     <section className="text-white flex size-full gap-10 flex-col">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <div className="bg-hero bg-cover h-[300px] w-full rounded-[20px]">
+        <div className="flex flex-col h-full justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          {/* TO DO: Make time to dynamic time */}
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList />
     </section>
   );
 }
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState } from "react";
+import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
+
+function MeetingTypeList() {
+  const router = useRouter();
+  const [meetingState, setmeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
+
+  // Create meeting room function
+  const createMeeting = () => {};
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      {/* Card */}
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setmeetingState("isInstantMeeting")}
+        className="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        handleClick={() => setmeetingState("isScheduleMeeting")}
+        className="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Checkout your recording"
+        handleClick={() => router.push("/recordings")}
+        className="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invitation link"
+        handleClick={() => setmeetingState("isJoiningMeeting")}
+        className="bg-yellow-1"
+      />
+
+      {/* Meeting Modal */}
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setmeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
+    </section>
+  );
+}
+
+export default MeetingTypeList;
